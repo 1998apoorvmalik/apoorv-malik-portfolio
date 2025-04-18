95bc51c03a057206184962e068f4105564baa6aa
Apoorv Malik <1998apoorvmalik@gmail.com>
2025-04-18 14:45:55 -0700

    finally working

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // base: '/apoorv-malik-portfolio/', // for GitHub Pages
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
